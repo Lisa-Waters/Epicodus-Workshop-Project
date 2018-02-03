f1d1ea63d6fcf54a6f6e4df7e1d754d9e4a95d68
Lisa_H2o2 <elsikorski920@gmail.com>
2018-02-03 14:05:19 -0800

    Some style polish

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   $("#goal-button").click(function() {
-    $("#goal-list").append("<li class='list-item'>" + $("#new-goal").val() + "</li>");
+    $("#goal-list").append("<li class='list-group-item'>" + $("#new-goal").val() + "</li>");
     $("#new-goal").val("");
   })
 })
